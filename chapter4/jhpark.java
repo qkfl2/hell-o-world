3ee54528fb3a05f0512760543ac9cdd5ea63076b
dry-p <quebostina@gmail.com>
2016-11-08 00:52:06 +0900

    Update jhpark.java

--- chapter4/jhpark.java
+++ chapter4/jhpark.java
@@ -5,9 +5,8 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Bibimbap(재료.고추장, 재료.된장, 재료.마요네즈).mix());
+        System.out.println(new 비빔밥(재료.고추장, 재료.된장, 재료.마요네즈).mix());
     }
-
 
     private enum 재료 implements Function<String, String> {
         고추장, 초장, 된장, 마요네즈, 와사비, 소주;
@@ -18,18 +17,18 @@ public class Main {
         }
     }
 
-    private static class Bibimbap {
+    private static class 비빔밥 {
         private final Function<String, String> materials;
 
         @SafeVarargs
-        Bibimbap(final Function<String, String>... input) {
+        비빔밥(final Function<String, String>... input) {
             materials = Stream.of(input)
                     .reduce(Function::compose)
                     .orElse(material -> material);
         }
 
         final String mix() {
-            return materials.apply("비빔밥");
+            return materials.apply(getClass().getSimpleName());
         }
     }
 
