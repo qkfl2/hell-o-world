9de0c77edf24b1e3481c4973826744f8708509c5
dry-p <quebostina@gmail.com>
2016-11-14 23:51:17 +0900

    Update jhpark.java

--- chapter5/jhpark.java
+++ chapter5/jhpark.java
@@ -6,7 +6,7 @@ import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.text.ParseException;
+import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.HashSet;
@@ -26,70 +26,57 @@ public class Main {
 
     }
 
-    private static class Fan {
+    private static final class Fan {
         static final Set<String> sEmails = new HashSet<>();
-        final String name;
-        final String email;
-        final int age;
-        final boolean isUniqueEmail;
 
-        static Stream<Fan> streamFromFiles(Stream<Path> pathStream) {
+        private final String name;
+        private final String email;
+        private final int age;
+        private final boolean isUniqueEmail;
+
+        private Fan(String name, String email, String birthday) {
+            this.name = name;
+            this.email = email;
+            this.age = Chapter5Helper.isValidDate(birthday)
+                    ? LocalDateTime.now().getYear() - Integer.valueOf(birthday.split("-")[0]) : 0;
+            this.isUniqueEmail = sEmails.add(email);
+        }
+
+        private static Stream<Fan> streamFromFiles(Stream<Path> pathStream) {
             return pathStream
                     .filter(Chapter5Helper::checkTestFile)
                     .flatMap(Chapter5Helper::fileToLines)
                     .filter(s -> !s.isEmpty())
-                    .map(Fan::parse);
+                    .map(s -> s.split("\\s+"))
+                    .map(parsed -> new Fan(parsed[0], parsed[1], parsed[2]));
         }
 
-        static Fan parse(String data) {
-            final String[] parsed = data.split("\\s+");
-            return new Fan(parsed[0], parsed[1], parsed[2]);
-        }
-
-        Fan(String name, String email, String birthday) {
-            this.name = name;
-            this.email = email;
-            this.age = Chapter5Helper.isValidDate(birthday) ? calcAge(birthday) : 0;
-            this.isUniqueEmail = sEmails.add(email);
-        }
-
-        void sendCongratulation(BufferedWriter writer) {
+        private void sendCongratulation(BufferedWriter writer) {
             try {
                 writer.write(this.name + "님 안녕하세요 당첨을 축하합니다!\n");
             } catch (IOException e) {
-                e.printStackTrace();
+                throw new UncheckedIOException(e);
             }
-        }
-
-        int calcAge(String birthday) {
-            return LocalDateTime.now().getYear() - Integer.valueOf(birthday.split("-")[0]);
         }
     }
 
-    interface Chapter5Helper {
-
-        static Stream<String> fileToLines(Path path) {
+    private static final class Chapter5Helper {
+        private static Stream<String> fileToLines(Path path) {
             try {
                 return Files.lines(path);
-            } catch (IOException e) { throw new UncheckedIOException(e); }
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
         }
 
-        static boolean checkTestFile(Path path) {
+        private static boolean checkTestFile(Path path) {
             return isValidDate(path.toString().replaceFirst("[.][^.]+$", ""));
         }
 
-        static boolean isValidDate(String inDate) {
+        private static boolean isValidDate(String inDate) {
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
             dateFormat.setLenient(false);
-
-            try {
-                dateFormat.parse(inDate.trim());
-            } catch (ParseException pe) {
-                return false;
-            }
-
-            return true;
+            return dateFormat.parse(inDate, new ParsePosition(0)) != null;
         }
     }
-
 }
