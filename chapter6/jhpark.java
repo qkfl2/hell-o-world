4d2ee5cf883c2b5d61506872e24be16b43fe2310
dry-p <quebostina@gmail.com>
2016-11-20 01:30:59 +0900

    Update jhpark.java

--- chapter6/jhpark.java
+++ chapter6/jhpark.java
@@ -8,13 +8,13 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
         Stream.iterate(new long[] {1, 1}, p -> new long[] {p[1], p[0] + p[1]})
-                .filter(i -> i[0] > 100000)
-                .filter(i -> isPrime(i[0]))
+                .filter(p -> p[0] > 100000)
+                .filter(p -> isPrime(p[0]))
                 .findFirst()
                 .ifPresent(i -> System.out.println(i[0]));
     }
 
-    private static boolean isPrime(final long number) {
-        return LongStream.rangeClosed(2, (long)(Math.sqrt(number))).allMatch(n -> number % n != 0);
+    private static boolean isPrime(final long p) {
+        return LongStream.rangeClosed(2, (long)(Math.sqrt(p))).allMatch(n -> p % n != 0);
     }
 }
