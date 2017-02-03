22a9c131017e1fd8d544da1675dd30b49a1f4547
dry-p <quebostina@gmail.com>
2017-02-03 23:29:12 +0900

    Create jhpark.scala

---
+++ chapter4/jhpark.scala
@@ -0,0 +1,13 @@
+object Chapter4 {
+  class 비빔밥 {  def 비벼(s: String): String = { s + "비빔밥" } }
+
+  trait 고추장 extends 비빔밥 { override def 비벼(s: String): String = { super.비벼("고추장 " + s) } }
+  trait 초장 extends 비빔밥 { override def 비벼(s: String): String = { super.비벼("초장 " + s) } }
+  trait 된장 extends 비빔밥 { override def 비벼(s: String): String = { super.비벼("된장 " + s) } }
+  trait 소주 extends 비빔밥 { override def 비벼(s: String): String = { super.비벼("소주 " + s) } }
+
+  def main(args: Array[String]): Unit = {
+    val 저녁밥 = new 비빔밥 with 고추장 with 된장 with 소주
+    println(저녁밥 비벼 "")
+  }
+}
